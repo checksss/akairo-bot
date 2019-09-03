8e413a7b73776e1699d4a22564a5004386cd38cd
Quentin Wright <wrightq00@gmail.com>
2019-09-02 19:55:21 -0500

    rename client file

--- bot.ts
+++ bot.ts
@@ -1,4 +1,4 @@
-import AkairoBotClient from './client/AkairoClient';
+import AkairoBotClient from './client/AkairoBotClient';
 require('dotenv').config();
 
 const client: AkairoBotClient = new AkairoBotClient({
--- dist/bot.js
+++ dist/bot.js
@@ -3,9 +3,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const AkairoClient_1 = __importDefault(require("./client/AkairoClient"));
+const AkairoBotClient_1 = __importDefault(require("./client/AkairoBotClient"));
 require('dotenv').config();
-const client = new AkairoClient_1.default({
+const client = new AkairoBotClient_1.default({
     owner: process.env.owner,
     token: process.env.token
 });
