da5bbea87ffee0a0ebe6e2c29c6933c02087a853
Quentin Wright <wrightq00@gmail.com>
2019-09-16 20:04:55 -0500

    fix bugs/add features

--- commands/util/blacklist.ts
+++ commands/util/blacklist.ts
@@ -19,27 +19,49 @@ export default class BlacklistCommand extends Command {
                     match: 'content',
                     type: 'user',
                     prompt: {
-                        start: (message: Message): string => `${message.author}, who would you like to blacklist/allow?`
+                        start: (message: Message): string => `${message.author}, who would you like to blacklist/allow?`,
+                        retry: (message: Message): string => `${message.author}, please provide a valid user.`
                     }
+                },
+                {
+                    id: 'globalFlag',
+                    type: 'flag',
+                    flag: ['-g', '--global']
                 }
             ]
         });
     }
 
-    public async exec(message: Message, { user }: {user: User}): Promise<Message | Message[]> {
+    public async exec(message: Message, { user, globalFlag }: { user: User, globalFlag: boolean }): Promise<Message | Message[]> {
+        const global: boolean = this.client.isOwner(message.author!) && globalFlag;
         const blacklist: Array<string | null> = await this.client.settings.get('global', 'blacklist', []);
-        if (blacklist.includes(user.id)) {
-            const index = blacklist.indexOf(user.id);
-            blacklist.splice(index, 1);
-            if (blacklist.length === 0) await this.client.settings.set('global', 'blacklist', []);
-            else this.client.settings.set('global', 'blacklist', blacklist);
+        if (global) {
+            if (blacklist.includes(user.id)) {
+                const index = blacklist.indexOf(user.id);
+                blacklist.splice(index, 1);
+                if (blacklist.length === 0) await this.client.settings.set('global', 'blacklist', []);
+                else this.client.settings.set('global', 'blacklist', blacklist);
+                const guildBlacklist: Array<string> = await this.client.settings.get(message.guild!, 'blacklist', []);
+                if (guildBlacklist.includes(user.id)) return message.util!.send(`${user}, you're still blacklisted locally.`);
+                return message.util!.send(`${user.tag}, welcome back.`);
+            }
+            blacklist.push(user.id);
+            await this.client.settings.set('global', 'blacklist', blacklist);
+            return message.util!.send(`${user}, shame on you.`);
+        } else {
+            if (blacklist.includes(user.id)) {
+                const index = blacklist.indexOf(user.id);
+                blacklist.splice(index, 1);
+                if (blacklist.length === 0) await this.client.settings.set(message.guild!, 'blacklist', []);
+                else this.client.settings.set(message.guild!, 'blacklist', blacklist);
+                const globalBlacklist: Array<string> = await this.client.settings.get('global', 'blacklist', []);
+                if (globalBlacklist.includes(user.id)) return message.util!.send(`${user}, you're still blacklisted globally.`);
+                return message.util!.send(`${user.tag}, welcome back.`);
+            }
+            blacklist.push(user.id);
+            await this.client.settings.set(message.guild!, 'blacklist', blacklist);
+            return message.util!.send(`${user}, you've been blacklist.`);
 
-            return message.util!.send(`${user.tag}, welcome back.`);
         }
-
-        blacklist.push(user.id);
-        await this.client.settings.set('global', 'blacklist', blacklist);
-
-        return message.util!.send(`${user.tag}, shame on you.`);
     }
 }
--- dist/commands/util/blacklist.js
+++ dist/commands/util/blacklist.js
@@ -19,26 +19,55 @@ class BlacklistCommand extends discord_akairo_1.Command {
                     match: 'content',
                     type: 'user',
                     prompt: {
-                        start: (message) => `${message.author}, who would you like to blacklist/allow?`
+                        start: (message) => `${message.author}, who would you like to blacklist/allow?`,
+                        retry: (message) => `${message.author}, please provide a valid user.`
                     }
+                },
+                {
+                    id: 'globalFlag',
+                    type: 'flag',
+                    flag: ['-g', '--global']
                 }
             ]
         });
     }
-    async exec(message, { user }) {
+    async exec(message, { user, globalFlag }) {
+        const global = this.client.isOwner(message.author) && globalFlag;
         const blacklist = await this.client.settings.get('global', 'blacklist', []);
-        if (blacklist.includes(user.id)) {
-            const index = blacklist.indexOf(user.id);
-            blacklist.splice(index, 1);
-            if (blacklist.length === 0)
-                await this.client.settings.set('global', 'blacklist', []);
-            else
-                this.client.settings.set('global', 'blacklist', blacklist);
-            return message.util.send(`${user.tag}, welcome back.`);
+        if (global) {
+            if (blacklist.includes(user.id)) {
+                const index = blacklist.indexOf(user.id);
+                blacklist.splice(index, 1);
+                if (blacklist.length === 0)
+                    await this.client.settings.set('global', 'blacklist', []);
+                else
+                    this.client.settings.set('global', 'blacklist', blacklist);
+                const guildBlacklist = await this.client.settings.get(message.guild, 'blacklist', []);
+                if (guildBlacklist.includes(user.id))
+                    return message.util.send(`${user}, you're still blacklisted locally.`);
+                return message.util.send(`${user.tag}, welcome back.`);
+            }
+            blacklist.push(user.id);
+            await this.client.settings.set('global', 'blacklist', blacklist);
+            return message.util.send(`${user}, shame on you.`);
         }
-        blacklist.push(user.id);
-        await this.client.settings.set('global', 'blacklist', blacklist);
-        return message.util.send(`${user.tag}, shame on you.`);
+        else {
+            if (blacklist.includes(user.id)) {
+                const index = blacklist.indexOf(user.id);
+                blacklist.splice(index, 1);
+                if (blacklist.length === 0)
+                    await this.client.settings.set(message.guild, 'blacklist', []);
+                else
+                    this.client.settings.set(message.guild, 'blacklist', blacklist);
+                const globalBlacklist = await this.client.settings.get('global', 'blacklist', []);
+                if (globalBlacklist.includes(user.id))
+                    return message.util.send(`${user}, you're still blacklisted globally.`);
+                return message.util.send(`${user.tag}, welcome back.`);
+            }
+            blacklist.push(user.id);
+            await this.client.settings.set(message.guild, 'blacklist', blacklist);
+            return message.util.send(`${user}, you've been blacklist.`);
+        }
     }
 }
 exports.default = BlacklistCommand;
