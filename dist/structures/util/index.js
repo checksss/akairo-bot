0d3b72f9a543546e9110b00c39ca85f4c15ab3de
Quentin Wright <wrightq00gmail.com>
2019-10-07 18:26:49 -0500

    add profanity list

--- dist/structures/util/index.js
+++ dist/structures/util/index.js
@@ -2,6 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var Logger_1 = require("./Logger");
 exports.Logger = Logger_1.Logger;
+var Profanities_1 = require("./Profanities");
+exports.Profanities = Profanities_1.Profanities;
 var Server_1 = require("./Server");
 exports.Server = Server_1.Server;
 var Utils_1 = require("./Utils");
